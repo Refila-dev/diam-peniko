41a07291deefe98e6e594f47913c45d4046c6baf
Refila-dev <hei.zoarisoa@gmail.com>
2026-03-04 20:24:27 +0300

    feat: implement register and login page

---
+++ app/fidirana/miditra/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from '@/app/components/ui/login-page'
+
+const page = () => {
+  return (
+    <div className='mt-20'>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default page
---
+++ app/fidirana/misoratra/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterPage from '../../components/ui/register-page';
+
+const page = () => {
+  return (
+    <div className='mt-20'>
+      <RegisterPage />
+    </div>
+  )
+}
+
+export default page
